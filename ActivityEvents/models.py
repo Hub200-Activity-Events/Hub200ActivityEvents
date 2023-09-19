d32e9ff3df59d585ad24b5a088196044c0db340f
Ahmed Zahir <az9305290@gmali.com>
2023-09-19 23:02:56 +0300

    second commit

--- ActivityEvents/models.py
+++ ActivityEvents/models.py
@@ -17,6 +17,7 @@ class Events(models.Model):
     description = models.CharField(max_length=50) 
     event_date = models.DateField(default=timezone.now().date(),null=True)
     location = models.CharField(max_length=50)
+    image = models.ImageField(upload_to='images/',null=True)
     creator = models.ForeignKey(User, on_delete=models.CASCADE)
     def __str__(self):
         return f"{self.title}"
